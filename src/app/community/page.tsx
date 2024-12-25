37cb311bdf7eab674b766cc2d4fe2f88f1cbb227
byun0-0 <isly7959@naver.com>
2024-12-26 00:59:38 +0900

    feat: notice, post 이미지 업로드 기능 추가

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -10,30 +10,26 @@ const CommunityPage = async () => {
   const posts = await getPostListRequest();
   return (
       <>
-        <div>
-          <section className="community-cover bg-amber-200 pt-16 pb-20 pl-10">
-            <div className="cover-content">
-              <h1 className="text-4xl font-bold pb-6">커뮤니티</h1>
-              <p className="text-lg mb-4">사람들과 소통하고 정보를 공유해보세요.</p>
-              <span className="cover-description text-base">
-              여러 건강 정보를 서로 나누어 보세요.
-            </span>
+        <div className="bg-gray-100 min-h-screen">
+          <div className="container mx-auto">
+            <div className="cover-content text-center pt-10">
+              <h1 className="text-4xl font-bold pb-6">커뮤니티 관리</h1>
             </div>
-          </section>
-          <section className="community-post-list mt-10">
-            <div className="post-list-header flex justify-between items-center mx-10 my-4">
-              <div className="text-lg font-bold">최신 글</div>
-              <Link
-                  className="post-write-button text-black px-4 py-2 rounded-lg hover:bg-blue-200 border border-gray-300"
-                  href="/community/write"
-              >
-                글쓰기
-              </Link>
-            </div>
-            <div className="post-list px-10">
-              <PostList posts={posts}/>
-            </div>
-          </section>
+            <section className="community-post-list mt-10">
+              <div className="post-list-header flex justify-between items-center mx-10 my-4">
+                <div className="text-lg font-bold">최신 글</div>
+                <Link
+                    className="post-write-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    href="/community/write"
+                >
+                  글쓰기
+                </Link>
+              </div>
+              <div className="post-list px-10 py-4 bg-white rounded-lg shadow">
+                <PostList posts={posts}/>
+              </div>
+            </section>
+          </div>
         </div>
       </>
   );
--- src/components/community/post/WritePost.tsx
+++ src/components/community/post/WritePost.tsx
@@ -1,12 +1,13 @@
 "use client";
 import createPostRequest from "@/utils/api/post/createPostRequest";
-import React, {useEffect, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 
 const WritePost = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [category, setCategory] = useState('');
+  const [file, setFile] = useState<File | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -17,7 +18,7 @@ const WritePost = () => {
     }
   }, [router]);
   const handle = async () => {
-    const response = await createPostRequest(title, content, category);
+    const response = await createPostRequest(title, content, category, file);
     if (response.status === 201) {
       const postNo = response.location.split('/').pop();
       router.push("/community/post/" + postNo);
@@ -25,6 +26,13 @@ const WritePost = () => {
       alert("글쓰기에 실패했습니다.");
     }
   }
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
   return (
       <>
         <div>
@@ -41,6 +49,13 @@ const WritePost = () => {
           <label htmlFor="content" className="block text-sm font-medium text-gray-700">내용</label>
           <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} rows={5}
                     className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></textarea>
+        </div>
+        <div>
+          <input
+              type="file"
+              onChange={handleFileChange}
+              className={"block w-full p-2 border"}
+          />
         </div>
         <div>
           <button onClick={handle}
